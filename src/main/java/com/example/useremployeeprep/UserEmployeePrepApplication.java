8fdddc33de486d567597c09f09b0578689069d19
asger <asgervb@gmail.com>
2024-09-03 12:50:42 +0200

    Initial commit

---
+++ src/main/java/com/example/useremployeeprep/UserEmployeePrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.useremployeeprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEmployeePrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserEmployeePrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/useremployeeprep/UserEmployeePrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.useremployeeprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEmployeePrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
